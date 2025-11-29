726cefc04c7605552e6f00b09a4f508b5c6743e4
Petr Stastny <petr.stastny01@gmail.com>
2025-11-29 12:37:35 +0100

    feat: pretty print ast

--- src/Parse/AST.hs
+++ src/Parse/AST.hs
@@ -3,6 +3,7 @@
 {-# OPTIONS_GHC -Wno-missing-export-lists #-}
 module Parse.AST where
 import Data.Data
+import Data.List (intercalate)
 
 type Program a = [FunDecl a]
 
@@ -39,13 +40,13 @@ data Stmt a
     }
     | Block a [Stmt a]
     | AssignmentStmt a (Expr a) (Expr a)
-    deriving (Show, Eq, Ord, Data, Typeable)
+    deriving (Eq, Ord, Data, Typeable)
 
 data UnOp
     = Deref
     | Ref
     | Alloc
-    deriving (Show, Eq, Ord, Data, Typeable)
+    deriving (Eq, Ord, Data, Typeable)
 
 data BiOp
     = Eq
@@ -54,7 +55,7 @@ data BiOp
     | Minus
     | Mul
     | Div
-    deriving (Show, Eq, Ord, Data, Typeable)
+    deriving (Eq, Ord, Data, Typeable)
 
 data Expr a
     = BiOp a BiOp (Expr a) (Expr a)
@@ -76,10 +77,10 @@ data Expr a
     | Array a [Expr a]
     | Number a Int
     | EIdentifier a Identifier
-    deriving (Show, Eq, Ord, Data, Typeable)
+    deriving (Eq, Ord, Data, Typeable)
 
 newtype Record a = Fields [(Identifier, Expr a)]
-    deriving (Show, Eq, Ord, Data, Typeable)
+    deriving (Eq, Ord, Data, Typeable)
 
 exprLoc :: Expr a -> a
 exprLoc (BiOp l _ _ _) = l
@@ -93,3 +94,44 @@ exprLoc (Parse.AST.Record l _) = l
 exprLoc (Array l _) = l
 exprLoc (Number l _) = l
 exprLoc (EIdentifier l _) = l
+
+instance Show UnOp where
+    show Deref = "*"
+    show Ref = "&"
+    show Alloc = "alloc"
+
+instance Show BiOp where
+    show Eq = "=="
+    show Gt = ">"
+    show Plus = "+"
+    show Minus = "-"
+    show Mul = "*"
+    show Div = "/"
+
+instance Show (Stmt a) where
+    show (OutputStmt _ e) = "output " <> show e
+    show (WhileStmt _ c b) = "while(" <> show c <> ") " <> show b
+    show (IfStmt _ c t f) = "if(" <> show c <> ") " <> show t <> show (fstr f)
+        where
+            fstr Nothing = ""
+            fstr (Just fb) = " else " <> show fb
+    show (Block _ stmx) = "{\n" <> intercalate "\n" (show <$> stmx) <> "\n}"
+    show (AssignmentStmt _ l r) = show l <> " = " <> show r
+
+instance Show (Expr a) where
+    show (BiOp _ o l r) = show l <> " " <> show o <> " " <> show r
+    show (UnOp _ u e) = show u <> " " <> show e
+    show (Input _) = "input"
+    show (Null _) = "null"
+    show (FieldAccess _ t f) = show t <> "." <> show f
+    show (ArrayAccess _ t i) = show t <> "[" <> show i <> "]"
+    show (Call _ t ax) = show t <> "(" <> intercalate "," (show <$> ax) <> ")"
+    show (Record _ rx) = show rx
+    show (Array _ ex) = "[" <> intercalate ", " (show <$> ex) <> "]"
+    show (Number _ i) = show i
+    show (EIdentifier _ i) = i
+
+instance Show (Record a) where
+    show (Fields fx) = "{" <> intercalate ", " (sf <$> fx) <> "}"
+        where
+            sf (i, e) = show i <> ": " <> show e
