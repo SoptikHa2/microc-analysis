fb5eb0c4666bdea535a1019e01bef61482948951
Petr Stastny <petr.stastny01@gmail.com>
2025-12-02 01:51:54 +0100

    vmoved with consts

--- src/Analysis/Dataflow/Const.hs
+++ src/Analysis/Dataflow/Const.hs
@@ -5,7 +5,9 @@ import qualified Data.Map as M
 import Parse.AST
 import Analysis.Dataflow.Lattices (ConstLattice)
 import Control.Monad.State
-import Lattice (Lattice(bottom))
+import Lattice (Lattice(..), (<&&>), (<||>))
+import Data.Maybe
+import Data.Foldable
 
 type ResultMap = M.Map CFGId ResultLat
 type ResultLat = M.Map Identifier ConstLattice
@@ -36,6 +38,7 @@ runStep toRun cfgMap = do
 -- node values in the map if anything changed, and return whether so
 runCfg :: CFGNode a -> State ResultMap Bool
 runCfg (FunEntry id _ vars _) = do
+    -- The entry is always out-of-date only once
     m <- get
     case m M.!? id of
         Nothing -> do
@@ -45,9 +48,53 @@ runCfg (FunEntry id _ vars _) = do
             pure True
         Just _ -> pure False
 
+runCfg (Node id _ prevs stmt) = do
+    -- 1) get assignments of all previous ones
+    -- 2) use the statement to compute new constraints
+    -- 3) replace self with new ones
+
+    -- assumption: there is at least one child with the items
+    prevAssignments <- catMaybes <$> gets (\m -> (m M.!?) <$> prevs)
+    let mergedPrev = foldr1 (<||>) prevAssignments
+
+    -- use the assumptions to compute self values
+    let mine = computeStmt stmt mergedPrev
+
+    existingSolution <- gets (M.!? id)
+
+    let changed = case existingSolution of
+                Just e | e == mine -> False
+                _ -> True
+    
+    modify (M.insert id mine)
+    
+    pure changed
 
 runCfg (FunExit id _ _ prevs) = do
     -- 1) get assignments of all previous ones
-    -- 2) <&> them
+    -- 2) <|> them
     -- 3) insert into map, return if was equialent
-    undefined
+
+    -- assumption: there is at least one child with the items
+    prevAssignments <- catMaybes <$> gets (\m -> (m M.!?) <$> prevs)
+    let mergedPrev = foldr1 (<||>) prevAssignments
+
+    existingSolution <- gets (M.!? id)
+
+    let changed = case existingSolution of
+                Just e | e == mergedPrev -> False
+                _ -> True
+    
+    modify (M.insert id mergedPrev)
+    
+    pure changed
+
+
+computeStmt :: Stmt a -> ResultLat -> ResultLat
+-- The only one that matters is Assignment. Block should not appear (this is CFG!)
+computeStmt (Block _ _) _ = error "Block statement remained in CFG. This is illegal"
+computeStmt (AssignmentStmt _ (EIdentifier _ variable) rhs) lat = undefined
+computeStmt _ lat = lat
+
+computeExpr :: Expr a -> ResultLat -> ConstLattice
+computeExpr _ _ = undefined
--- src/Lattice.hs
+++ src/Lattice.hs
@@ -1,4 +1,5 @@
-module Lattice (Lattice(..)) where
+module Lattice (Lattice(..), (<&&>), (<||>)) where
+import qualified Data.Map as M
 
 class Eq a => Lattice a where
     top :: a
@@ -6,3 +7,9 @@ class Eq a => Lattice a where
     (<&>) :: a -> a -> a
     (<|>) :: a -> a -> a
 
+
+(<&&>) :: Ord a => Lattice b => M.Map a b -> M.Map a b -> M.Map a b
+lhs <&&> rhs = M.unionWith (<&>) lhs rhs
+
+(<||>) :: Ord a => Lattice b => M.Map a b -> M.Map a b -> M.Map a b
+lhs <||> rhs = M.unionWith (<|>) lhs rhs
